a7ca9bb26fbb941cd1e726c8691e45a1d73035d6
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-09-12 17:28:06 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dotNet_ass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Login login = new Login();
+            while (!login.LoginSuccess & !login.Exit)
+            {
+                login.LoginScreen();
+            }
+            Console.WriteLine(login.LoginSuccess);
+            Console.WriteLine("after login");
+        }
+    }
+}
