ed089b682e96bb14aea3149de71fb7c502b17efc
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-09-12 17:42:59 +1000

    added layout for login page

--- Login.cs
+++ Login.cs
@@ -1,18 +1,52 @@
 ﻿using System;
+using System.IO;
 
 namespace dotNet_ass1
 {
-    class Program
+    class Login
     {
-        static void Main(string[] args)
+        private string userName, passWord;
+        private bool loginSuccess = false;
+        private bool exit = false;
+        public string Name
         {
-            Login login = new Login();
-            while (!login.LoginSuccess & !login.Exit)
+            get
             {
-                login.LoginScreen();
+                return this.userName;
             }
-            Console.WriteLine(login.LoginSuccess);
-            Console.WriteLine("after login");
+        }
+        public bool LoginSuccess
+        {
+            get
+            {
+                return this.loginSuccess;
+            }
+        }
+        public bool Exit
+        {
+            get
+            {
+                return this.exit;
+            }
+        }
+        public void LoginScreen()
+        {
+            Console.WriteLine("         ------------------------------------------------------------- ");
+            Console.WriteLine("        |                Welcome to KAZ Banking System                |");
+            Console.WriteLine("         =============================================================");
+            Console.WriteLine("        |                        Please Login                         |");
+            Console.WriteLine("        |                                                             |");
+            Console.Write("        | User name: ");
+            int usernameLeft = Console.CursorLeft;
+            int usernameTop = Console.CursorTop;
+            Console.Write("                                                 |\n");
+            Console.Write("        | Password: ");
+            int passwordLeft = Console.CursorLeft;
+            int passwordTop = Console.CursorTop;
+            Console.Write("                                                  |\n");
+            Console.WriteLine("        |                                                             |");
+            Console.WriteLine("         ------------------------------------------------------------- ");
+            Console.ReadKey();
         }
     }
 }
